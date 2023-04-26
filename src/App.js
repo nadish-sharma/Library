e3c297a700b598f59305c0b2faee5f397688edd0
nadish-sharma <nadishsharma1@gmail.com>
2023-04-25 18:34:36 -0600

    google books search api added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import UserList from './components/UserList';
+import GetUser from './routes/GetUser';
+import AddUser from './routes/AddUser';
+import UpdateUser from './routes/UpdateUser';
+import DeleteUser from './routes/DeleteUser';
+import LoginForm from './routes/LoginForm';
+import Home from './routes/Home';
+import BookList from './routes/BookList';
+import BookDescription from './routes/BookDescription';
+import 'font-awesome/css/font-awesome.min.css';
+import AppContext from './AppContext';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [bookData, setBookData] = useState([]);
+  const [admin, setadmin] = useState(false);
+  const [users, setUsers] = useState('');
+  const handleGetUsersByAdminStatus = () => {
+    axios.get(`/api/user/users?admin=${admin}`)
+   .then(response => setUsers(response.data))
+   .catch(error => console.error(error));
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <AppContext.Provider value={{ bookData, setBookData }}>
+    <BrowserRouter>
+      <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/getUser" element={<GetUser />} />
+            <Route path="/addUser" element={<AddUser />} />
+            <Route path="/updateUser" element={<UpdateUser />} />
+            <Route path="/deleteUser" element={<DeleteUser />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/bookdescription" element={<BookDescription />} />
+            <Route path="/booklist" element={<BookList />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
+  </AppContext.Provider>
   );
 }
 
